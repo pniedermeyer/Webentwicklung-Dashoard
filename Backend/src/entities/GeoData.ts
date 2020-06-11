4211ed26d8e052b1946717cb6c63d83dca18b008
Lukas Raubuch <lukas.raubuch@web.de>
2020-06-11 13:32:52 +0200

    Reverse engineering via typeorm-model-generator

---
+++ Backend/src/entities/GeoData.ts
@@ -0,0 +1,23 @@
+import { Column, Entity, Index } from "typeorm";
+
+@Index("geo_data_pkey", ["blId", "lkId", "res", "ringId"], { unique: true })
+@Entity("geo_data", { schema: "public" })
+export class GeoData {
+  @Column("integer", { primary: true, name: "bl_id" })
+  blId: number;
+
+  @Column("integer", { primary: true, name: "lk_id" })
+  lkId: number;
+
+  @Column("integer", { primary: true, name: "ring_id" })
+  ringId: number;
+
+  @Column("integer", { primary: true, name: "res" })
+  res: number;
+
+  @Column("float8", { name: "x", array: true })
+  x: number[];
+
+  @Column("float8", { name: "y", array: true })
+  y: number[];
+}
