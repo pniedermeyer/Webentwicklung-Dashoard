5b2c05757d9437fd282d099183b560532b63e493
Tim Winter <t.winter.2@gmx.com>
2020-06-12 16:13:33 +0200

    bootstrap 3 Columns Layout

--- Frontend/dashboard/src/App.vue
+++ Frontend/dashboard/src/App.vue
@@ -1,59 +1,29 @@
 <template>
   <div id="app">
-    <bar-chart :chartData="arrTotal" :options="chartOptions" :label="Test"></bar-chart> 
-
-    <grid-layout
-                    :layout.sync="layout"
-                    :col-num="12"
-                    :row-height="30"
-                    :is-draggable="true"
-                    :is-resizable="true"
-                    :vertical-compact="true"
-                    :margin="[10, 10]"
-                    :use-css-transforms="true"
-    >
-
-            <grid-item v-for="item in layout"
-                                  :x.sync="item.x"
-                                  :y.sync="item.y"
-                                  :w.sync="item.w"
-                                  :h.sync="item.h"
-                                  :i="item.i"
-                                  :key="item.i">
-                    {{item.i}}
-            </grid-item>
-    </grid-layout>
+    <b-container class="bv-example-row">
+      <b-row>
+        <b-col>1 of 3: 
+          <bar-chart :chartData="arrTotal" :options="chartOptions" :label="Test"></bar-chart>
+        </b-col>
+        <b-col>2 of 3</b-col>
+        <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
   </div> 
 </template>
 
-<script src="vue-grid-layout.umd.min.js"></script>
 <script>
-import VueGridLayout from 'vue-grid-layout';
-import Vue from 'vue' // es6 syntax
-import Test from './components/Test.vue'
-// import BarChart from './components/Barchart.vue'
 import BarChart from './components/BarchartTest.vue'
 
-var testLayout = [
-    {"x":0,"y":0,"w":2,"h":2,"i":"0"},
-    {"x":2,"y":0,"w":2,"h":4,"i":"1"},
-    {"x":4,"y":0,"w":2,"h":5,"i":"2"},
-    {"x":6,"y":0,"w":2,"h":3,"i":"3"},
-    {"x":8,"y":0,"w":2,"h":3,"i":"4"},
-    {"x":10,"y":0,"w":2,"h":3,"i":"5"}
-];
 
 export default {
   name: 'App',
   components: {
-    GridLayout: VueGridLayout.GridLayout,
-    GridItem: VueGridLayout.GridItem,
     BarChart
   },
   data () {
     return {
       Test: 'Cases100k',
-      layout: testLayout,
       arrTotal: [10,15,20],
       arrCounty: ['Saarbrücken', 'Saarlouise', 'Merzig'],
       chartOptions: {
