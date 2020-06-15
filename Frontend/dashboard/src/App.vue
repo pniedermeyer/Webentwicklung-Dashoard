bc6697d0297772c6df54c9debb388fe9b64da4f2
Simon <simon.triem@yahoo.de>
2020-06-15 11:15:51 +0200

    Chart Bar
    Co-authored-by: pniedermeyer <36039453+pniedermeyer@users.noreply.github.com>
    Co-authored-by: Grossdrache <philipp@tull.de>

--- Frontend/dashboard/src/App.vue
+++ Frontend/dashboard/src/App.vue
@@ -3,12 +3,13 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col>1 of 3: 
-          <bar-chart :chartData="arrTotal" :options="chartOptions" :label="Test"></bar-chart>
+          <bar-chart :test="test"></bar-chart>
         </b-col>
         <b-col>2 of 3
             <MapSVG> </MapSVG>
         </b-col>
-        <b-col>3 of 3</b-col>
+        <b-col>3 of 3
+        </b-col>
       </b-row>
     </b-container>
   </div> 
@@ -18,7 +19,6 @@
 import BarChart from './components/BarchartTest.vue'
 import MapSVG from './components/MapSVG.vue'
 
-
 export default {
   name: 'App',
   components: {
@@ -27,21 +27,7 @@ export default {
   },
   data () {
     return {
-      Test: 'Cases100k',
-      arrTotal: [10,15,20],
-      arrCounty: ['Saarbrücken', 'Saarlouise', 'Merzig'],
-      chartOptions: {
-            scales: {
-        xAxes: [{
-            ticks: {
-                beginAtZero: true
-            }
-        }]
-    },
-        responsive: true,
-        maintainAspectRatio: false,
-
-      }
+      test: "HIPASCAL"
     }
   }
 }
