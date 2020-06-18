cb6c79f4aa3b1d767943bdc8c8878849686b68f2
cedi.schreiner@web.de <qZX59V5Bt5EY9gEz4zgL>
2020-06-18 12:24:57 +0200

    -

--- Frontend/dashboard/src/components/MapSVG.vue
+++ Frontend/dashboard/src/components/MapSVG.vue
@@ -14,7 +14,7 @@
       <div>
         <!-- zoomlevel -->
         <label for="zoom">Zoomlevel:</label>
-        <input type="number" v-model="zoom" step="20" min="0" v-on:change="zoomLevelChanged()"/>
+        <input type="number" v-model="zoom" step="20" min="0" v-on:change="zoomLevelChanged()" />
       </div>
     </div>
 
@@ -25,96 +25,110 @@
 </template>
 
 <script>
-import axios from "axios";
+import axios from 'axios'
 
 export default {
-  name: "MapSVG",
+  name: 'MapSVG',
   data() {
     return {
       zoom: 100,
       resolution: 0,
       resolutions: [
-        { text: "Niedrig", value: 0 },
-        { text: "Mittel", value: 1},
-        { text: "Hoch", value: 2}
+        { text: 'Niedrig', value: 0 },
+        { text: 'Mittel', value: 1 },
+        { text: 'Hoch', value: 2 },
       ],
       selectedBL: null,
-      selectedLK: null
-    };
+      selectedLK: null,
+    }
   },
   props: {
     LK_ID: Number,
-    BL_ID: Number
+    BL_ID: Number,
   },
   created() {
-    this.fetchGeoData();
+    this.fetchGeoData()
   },
   methods: {
     fetchGeoData(res = 0) {
-      let that = this;
-      let url = `http://localhost:3001/geodata?` + 'res=' + res;
+      let that = this
+
+      // const request = {
+      //   url: `http://localhost:3001/geodata?` + 'res=' + res,
+      //   method: 'get',
+      //   headers: {
+      //     'Access-Control-Allow-Origin': 'http://localhost:3001',
+      //   },
+      // }
+      let url = `http://localhost:3001/geodata?` + 'res=' + res
       console.log(url)
+      // axios
+      //   .request(request)
+      //   .then(function(response) {
+      //     console.log('fetch data success' + response)
+      //     that.processMapData(response.data)
+      //   })
+      //   .catch(function(error) {
+      //     console.log('fetch data error:' + error)
+      //   })
       axios
         .get(url)
         .then(function(response) {
-          console.log("fetch data success" + response);
-          that.processMapData(response.data);
+          console.log('fetch data success' + response)
+          that.processMapData(response.data)
         })
         .catch(function(error) {
-          console.log("fetch data error:" + error);
-        });
+          console.log('fetch data error:' + error)
+        })
     },
     zoomLevelChanged() {
-      const mapSvg = document.getElementById("map_area");
-      mapSvg.setAttribute("width", this.zoom);
+      const mapSvg = document.getElementById('map_area')
+      mapSvg.setAttribute('width', this.zoom)
     },
     resolutionChanged() {
       this.fetchGeoData(this.resolution)
     },
     processMapData(mapData) {
-      const svg = document.getElementById("map_area");
-      svg.innerHTML = "";
-      this.drawMapData(mapData);
+      const svg = document.getElementById('map_area')
+      svg.innerHTML = ''
+      this.drawMapData(mapData)
 
-      const svgBbox = svg.getBBox();
-      svg.setAttribute("viewBox", svgBbox.x + " " + svgBbox.y + " " + svgBbox.width + " " + svgBbox.height);
-      svg.setAttribute("width", this.zoom);
+      const svgBbox = svg.getBBox()
+      svg.setAttribute('viewBox', svgBbox.x + ' ' + svgBbox.y + ' ' + svgBbox.width + ' ' + svgBbox.height)
+      svg.setAttribute('width', this.zoom)
     },
     drawMapData(data) {
-      const mapSvg = document.getElementById("map_area");
+      const mapSvg = document.getElementById('map_area')
 
       if (!Array.isArray(data)) {
-        data = [data];
+        data = [data]
       }
 
-      data.forEach(state => {
-        state.counties.forEach(county => {
-          county.geometry.rings.forEach(ring => {
-            var pathString = "";
-            ring.forEach(coordinate => {
-              pathString += "L " + coordinate.x + "," + coordinate.y + " ";
-            });
-            pathString += "Z";
-            pathString = "M" + pathString.substring(1);
+      data.forEach((state) => {
+        state.counties.forEach((county) => {
+          county.geometry.rings.forEach((ring) => {
+            var pathString = ''
+            ring.forEach((coordinate) => {
+              pathString += 'L ' + coordinate.x + ',' + coordinate.y + ' '
+            })
+            pathString += 'Z'
+            pathString = 'M' + pathString.substring(1)
 
-            var path = this.createSVGElement("path", {
-              d: pathString
-            });
-            mapSvg.appendChild(path);
-          });
-        });
-      });
+            var path = this.createSVGElement('path', {
+              d: pathString,
+            })
+            mapSvg.appendChild(path)
+          })
+        })
+      })
     },
     createSVGElement(name, attributes) {
-      var element = document.createElementNS(
-        "http://www.w3.org/2000/svg",
-        name
-      );
+      var element = document.createElementNS('http://www.w3.org/2000/svg', name)
       for (const attName in attributes) {
-        element.setAttribute(attName, String(attributes[attName]));
+        element.setAttribute(attName, String(attributes[attName]))
       }
-      return element;
-    }
-  }
-};
+      return element
+    },
+  },
+}
 </script>
