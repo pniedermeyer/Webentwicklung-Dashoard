e91e721ee1bc03d35d90c71823277db57f5e766e
Lukas Raubuch <lukas.raubuch@web.de>
2020-06-24 10:44:02 +0200

    Data Route is now accessible via querystring parameters

--- Backend/src/controllers/InfectionsController.ts
+++ Backend/src/controllers/InfectionsController.ts
@@ -7,18 +7,31 @@ import RkiDataAPI from '../map-data-manager/data-requests/rki-data-request'
 class InfectionsController {
   static async infectionData(req: Request, res: Response) {
     let infections
+    const query: any = { where: {}}
+    if (req.query.blid !== undefined) {
+      query.where.blId = req.query.blid
+    }
+    if (req.query.lkid !== undefined) {
+      query.where.lkId = req.query.lkid
+    }
+    if (req.query.date !== undefined) {
+      query.where.date = req.query.date
+    } else {
+      query.where.date = getCurrentDate()
+    }
+    console.log(query)
     try {
-      //   infections = await getConnection()
-      //     .getRepository(Infections)
-      //     .find({ where: { blId: req.query.blid, lkId: req.query.lkid, date: req.query.date } })
-      //   res.send(infections)
+      infections = await getConnection()
+        .getRepository(Infections)
+        .find(query)
+      res.send(infections)
     } catch (error) {
       console.log(error)
       res.status(401).send('FEHLER')
     }
 
     //Nur vorrübergehende Lösung bis die Anbundung an die Datenbank fertig ist. So kann das Frontend mit aktuellen Daten arbeiten
-    res.send(await RkiDataAPI.get())
+    //res.send(await RkiDataAPI.get())
   }
 
   static async writeInfections() {
