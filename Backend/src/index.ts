98d46d3064b6ba20216a411019e80f33389b28f6
Lukas Raubuch <lukas.raubuch@web.de>
2020-06-26 09:55:15 +0200

    Ready for production

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,8 +5,7 @@ import routes from './routes'
 import cors from 'cors'
 import InfectionsController from './controllers/InfectionsController'
 import Scheduler from './map-data-manager/scheduler/scheduler'
-
-// writeGeoDataInResolutions()
+import writeGeoDataInResolutions from './lib/db-imports'
 
 createConnection()
   .then((connection) => {
@@ -18,17 +17,12 @@ createConnection()
 
     // Set all routes from routes folder
     app.use('/', routes)
-
+    
     app.listen(3001, () => {
       console.log('Server started on port 3001!')
-      console.log('Starting Scheduler...')
-      let infectionDataScheduler: Scheduler = new Scheduler(st, { schedule: '*/10 * * * * *' })
+      let infectionDataScheduler: Scheduler = new Scheduler(InfectionsController.writeInfections, { schedule: '0 0 1 * * *' })
       infectionDataScheduler.start()
       console.log('Scheduler started')
     })
   })
   .catch((error) => console.log(error))
-
-function st() {
-  console.log('Scheduler running test...')
-}
