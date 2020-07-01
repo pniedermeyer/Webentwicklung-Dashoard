e4c4b2c3f8b4ee978854516673dbc8e2be93052f
cedi.schreiner@web.de <qZX59V5Bt5EY9gEz4zgL>
2020-07-01 21:32:43 +0200

    Bugfix

--- Backend/src/entity/GeoDataObject.ts
+++ Backend/src/entity/GeoDataObject.ts
@@ -10,3 +10,26 @@ export class GeoDataObject {
   @Column('jsonb')
   geojson!: FeatureCollection
 }
+
+/*
+geodata CREATE TABLE SCRIPT
+-- Table: public.geodata
+
+-- DROP TABLE public.geodata;
+
+CREATE TABLE public.geodata
+(
+    bl_id integer NOT NULL,
+    lk_id integer NOT NULL,
+    x double precision[] NOT NULL,
+    y double precision[] NOT NULL,
+    ring_id integer NOT NULL,
+    res integer NOT NULL,
+    CONSTRAINT geodata_pkey PRIMARY KEY (bl_id, lk_id, ring_id, res)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.geodata
+    OWNER to postgres;
+*/
