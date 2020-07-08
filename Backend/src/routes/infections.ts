5a93ebfaec0501a0a2d45e1ac5b41393ee724585
Lukas Raubuch <lukas.raubuch@web.de>
2020-07-08 16:05:17 +0200

    Commented each route file

--- Backend/src/routes/infections.ts
+++ Backend/src/routes/infections.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import InfectionsController from '../controllers/InfectionsController'
 
+/**
+ * Router to be used for infections data requests in the express app.
+ */
 const router = Router()
 
 router.get('/', InfectionsController.infectionData)
--- Backend/src/routes/settings.ts
+++ Backend/src/routes/settings.ts
@@ -1,6 +1,9 @@
 import { Router } from 'express'
 import SettingsController from '../controllers/SettingsController'
 
+/**
+ * Router to be used for settings data requests in the express app.
+ */
 const router = Router()
 
 router.get('/', SettingsController.settings)
