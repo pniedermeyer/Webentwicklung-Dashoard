656a43b1bf83fd8cf64af04ca279b2b9c2862aa7
Lukas Reichmann <lukas@reichmann-info.de>
2020-07-10 16:38:50 +0200

    Added scheduled cleanup routines.

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -5,6 +5,8 @@ import routes from './routes'
 import cors from 'cors'
 import InfectionsController from './controllers/InfectionsController'
 import Scheduler from './utilities/scheduler'
+import cleanupInfections from './utilities/infections-cleanup'
+import cleanupSettings from './utilities/settings-cleanup'
 
 createConnection()
   .then((connection) => {
@@ -23,6 +25,9 @@ createConnection()
       console.log('Server started on port 3001!')
       const infectionDataScheduler = new Scheduler(InfectionsController.writeInfections, { schedule: '0 0 1 * * *' })
       infectionDataScheduler.start()
+      new Scheduler(cleanupInfections, {schedule: '0 0 2 * * *'}).start();
+      new Scheduler(cleanupSettings, {schedule: '0 0 2 * * *'}).start();
+
       console.log('Scheduler started')
     })
   })
