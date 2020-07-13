96c6958de04ee4e9b4620d45721f06fa2fadec0c
Lukas Raubuch <lukas.raubuch@web.de>
2020-07-13 13:21:02 +0200

    Fixing typos all over the place

--- Backend/src/entity/GeoDataObject.ts
+++ Backend/src/entity/GeoDataObject.ts
@@ -2,9 +2,9 @@ import { Column, Entity, Index } from 'typeorm'
 import { FeatureCollection } from 'geojson'
 
 /**
- * Entity class for geo data.
+ * Entity class for GeoJSON data.
  * When querying the database we want to query for a specific resolution.
- * The geojson column contains the geojson which we want to provide.
+ * The GeoJSON column contains the geojson which we want to provide.
  */
 @Index('geo_test_pkey', ['res'], { unique: true })
 @Entity('geodataobjects', { schema: 'public' })
